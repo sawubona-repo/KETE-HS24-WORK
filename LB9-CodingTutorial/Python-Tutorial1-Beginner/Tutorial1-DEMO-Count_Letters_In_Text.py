f2641fd7aaeac45cfc8735d18fb22da17a71c9b5
sawubona <mail@sawubona.ch>
2024-11-19 16:02:14 +0100

    Update Tutorial1-DEMO-Count_Letters_In_Text.py

--- LB9-CodingTutorial/Python-Tutorial1-Beginner/Tutorial1-DEMO-Count_Letters_In_Text.py
+++ LB9-CodingTutorial/Python-Tutorial1-Beginner/Tutorial1-DEMO-Count_Letters_In_Text.py
@@ -1,28 +1,23 @@
 # =================================================================
-# Lists and Sorting
+# DEMO: Lists and Letter Counting
 # =================================================================
-# List of integer numbers and sorting with the classical 
-# "bubble sort" algorithm 
-#
-# See: https://de.wikipedia.org/wiki/Bubblesort
+# Count letters in a given text e.g. for a frequency analysis
 # -----------------------------------------------------------------
 # Nov'2023 - dbe - initial version for KETE HS23
+# Nov'2024 - dbe - minor corrections for KETE HS24
 # -----------------------------------------------------------------
 
-# ------------------------------------------------------------------
-import random
+# -----------------------------------------------------------------
+# Import Libraries
+# -----------------------------------------------------------------
 
-
-# =================================================================
-# CASE 3: 
-
+# -----------------------------------------------------------------
+# Define Helper Functions/Subroutines
+# -----------------------------------------------------------------
 def count_letter(letter,list):
-  #charNo = ord(letter.lower())-97
   charNo = ord(letter)
-  #print(letter, charNo)
   if (charNo > 0):
     list[charNo] += 1
-
 
 def init_letter_counter(no):
   cList = []
@@ -30,31 +25,43 @@ def init_letter_counter(no):
     cList.append(0)
   return cList
 
-letter_counter = init_letter_counter(255)
-# print("initial counter: ", letter_counter)
+# -----------------------------------------------------------------
+# MAIN PROGRAM
+# -----------------------------------------------------------------
 
-# text  = "This is a simple Text with not other Meaning"
+# Die counter list variable initialisieren
+letter_counter = init_letter_counter(255)
+print("Initial counter list: \n", letter_counter)
+
+# -----------------------------------------------------------------
+# INPUT Data
+
+text  = "This is a simple Text with no other meaning than be a Text for testing the Letter Counter Program"
 # text = input("Ihr gewünschter Text? ")
 
-
-myFile = open("sampletext.txt", "r")
-text = myFile.read()
-myFile.close()
-
-print ("\n\nDer zu analysierende Text:\n" + text)
+# myFile = open("sampletext.txt", "r")
+# text = myFile.read()
+# myFile.close()
 
 # -------------------------------------------------------
-# Count frequency of characters in a text
+# PROCESS Data: Count frequency of characters in a given text
+
 count = 0
 while (count < len(text)):
   count_letter(text[count],letter_counter)
   count +=1
-# -------------------------------------------------------
 
-print ("\nund dessen Buchstaben Häufigkeiten: ")
-for i in range(255):
-  print(chr(i+97),letter_counter[i])  
-  
+
+# -----------------------------------------------------------------
+# OUPUT Data/Results
+
+print ("\n\nDer zu analysierende Text:\n" + text)
+print ("\nund die Häufigkeiten der darin enthaltenen Buchstaben: ")
+
+for i in range(32,255):
+  if letter_counter[i]>0:
+    print(chr(i),letter_counter[i])  
+
 # print("letter counter: ", letter_counter)
 print("\nTotal "+ str(sum(letter_counter)) +" Buchstaben")  
 
